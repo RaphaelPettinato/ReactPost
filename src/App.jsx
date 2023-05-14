4ed350d10ec594d048d33054d1604eb6cc9babc0
Raphael Costa Pettinato <raphael.petinatti@gmail.com>
2023-05-14 17:31:06 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post"
+
+function App() {
+
+  return (
+    <Post 
+    author="Raphael Costa Pettinato" 
+    content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus sapiente quos, perspiciatis ipsa animi rem dicta culpa consectetur? Quia recusandae ducimus officiis nam quae libero sint eius a facilis! Modi?"
+    />
+  )
+}
+
+export default App
